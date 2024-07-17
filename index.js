ad0ea2e7e09226962f6e36cfdfed36b59bb3eb22
Yeasin <ar9485727@gmail.com>
2024-07-17 22:51:35 +0600

    test2

--- index.js
+++ index.js
@@ -9,18 +9,12 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "https://job-1-one.vercel.app"];
+const corsOptions = {
+  origin: ["http://localhost:5173","https://job-1-one.vercel.app"],
+  credentials: true,
+};
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
